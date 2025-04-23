24840bc2b768a90159c96152079104dc8f5a3142
wither19 <146484561+Wither19@users.noreply.github.com>
2025-04-23 15:53:10 +0000

    c

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/brine-pool-project/"
+  base: "/brine-pool-project/",
+  build: {
+    outDir: "./docs",
+    emptyOutDir: true
+  }
 })
